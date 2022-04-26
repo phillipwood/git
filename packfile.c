f8c3755d9002786b9f35808b308a06d802134a23
Phillip Wood <phillip.wood@dunelm.org.uk>
2022-04-26 16:42:36 +0100

    packfile: use crc_offset when indexing offset tables
    629dffc461 ("packfile: compute and use the index CRC offset",
    2020-05-25) noted that .... Use it
    
    Avoid truncation ...
    
    Signed-off-by: Phillip Wood <phillip.wood@dunelm.org.uk>

--- packfile.c
+++ packfile.c
@@ -1948,16 +1948,17 @@ off_t nth_packed_object_offset(const struct packed_git *p, uint32_t n)
 {
 	const unsigned char *index = p->index_data;
 	const unsigned int hashsz = the_hash_algo->rawsz;
-	index += 4 * 256;
 	if (p->index_version == 1) {
+		index += 4 * 256;
 		return ntohl(*((uint32_t *)(index + (hashsz + 4) * (size_t)n)));
 	} else {
 		uint32_t off;
-		index += 8 + (size_t)p->num_objects * (hashsz + 4);
-		off = ntohl(*((uint32_t *)(index + 4 * n)));
+		size_t tablesz = (size_t)p->num_objects * 4;
+		index += p->crc_offset + tablesz;
+		off = ntohl(*((uint32_t *)(index + 4 * (size_t)n)));
 		if (!(off & 0x80000000))
 			return off;
-		index += (size_t)p->num_objects * 4 + (off & 0x7fffffff) * 8;
+		index += tablesz + (size_t)off * 8;
 		check_pack_index_ptr(p, index);
 		return get_be64(index);
 	}
