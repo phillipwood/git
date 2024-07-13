f5d52114ef626f3e6c8002ce8c0c23c0d5110883
Karthik Nayak <karthik.188@gmail.com>
2024-07-13 15:45:18 +0200

    ci/style-check: add `RemoveBracesLLVM` to '.clang-format'
    For 'clang-format' setting  'RemoveBracesLLVM' to 'true', adds a check
    to ensure we avoid curly braces for single-statement bodies in
    conditional blocks.
    
    However, the option does come with two warnings [1]:
    
        This option will be renamed and expanded to support other styles.
    
    and
    
        Setting this option to true could lead to incorrect code formatting
        due to clang-format’s lack of complete semantic information. As
        such, extra care should be taken to review code changes made by
        this option.
    
    The latter seems to be of concern. While we want to experiment with the
    rule, adding it to the in-tree '.clang-format' could affect end-users.
    Let's only add it to the CI jobs for now. With time, we can evaluate
    its efficacy and decide if we want to add it to '.clang-format' or
    retract it entirely.
    
    [1]: https://clang.llvm.org/docs/ClangFormatStyleOptions.html#removebracesllvm
    
    Signed-off-by: Karthik Nayak <karthik.188@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ci/run-style-check.sh
+++ ci/run-style-check.sh
@@ -5,4 +5,17 @@
 
 baseCommit=$1
 
+# Remove optional braces of control statements (if, else, for, and while)
+# according to the LLVM coding style. This avoids braces on simple
+# single-statement bodies of statements but keeps braces if one side of
+# if/else if/.../else cascade has multi-statement body.
+#
+# As this rule comes with a warning [1], we want to experiment with it
+# before adding it in-tree. since the CI job for the style check is allowed
+# to fail, appending the rule here allows us to validate its efficacy.
+# While also ensuring that end-users are not affected directly.
+#
+# [1]: https://clang.llvm.org/docs/ClangFormatStyleOptions.html#removebracesllvm
+echo "RemoveBracesLLVM: true" >> .clang-format
+
 git clang-format --style file --diff --extensions c,h "$baseCommit"
