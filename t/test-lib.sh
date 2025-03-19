487d1a33130cb2fafadcf98da00a332a7408a0e8
Phillip Wood <phillip.wood@dunelm.org.uk>
2025-03-19 10:35:22 +0000

    breaking-changes: switch default branch to main
    Since 1296cbe4b46 (init: document `init.defaultBranch` better,
    2020-12-11) "git-init.adoc" has advertized that the default name
    of the initial branch may change in the future. The name "main"
    is chosen to match the default used by the big git forge web sites.
    
    The advice printed when init.defaultBranch is not set is
    updated to say that the default will change to "main" in Git
    3.0. Building with WITH_BREAKING_CHANGES enabled removes the advice,
    changes the default branch name is "main" and removes support for
    GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME. The code in guess_remote_head()
    that looks for "refs/heads/master" is left unchanged as that is only
    called when the remote server does not support the symref capability
    in the v0 protocol or the symref extension to the ls-refs list in the
    v2 protocol. Such an old server is more likely to be using "master"
    as the default branch name.
    
    With the exception of the "git-init.adoc" the documentation is left
    unchanged. I had hoped to parameterize the name of the default branch
    by using an asciidoc attribute. Unfortunately attribute expansion
    is inhibited by backticks and we use backticks to mark up ref names
    so that idea does not work. As the changes to git-init.adoc show
    inserting ifdef's around each instance of the branch name "master"
    is cumbersome and makes the documentation sources harder to read.
    
    Apart from "git-init.adoc" there are some other files where "master" is
    used as the name of the initial branch rather than as an example of a
    branch name such as "user-manual.adoc" and "gitcore-tutorial.adoc". The
    name appears a lot in those so updating it with ifdef's is not really
    practical. We can update that document in the 3.0 release cycle. The
    other documentation where master is used as an example branch name
    can be gradually converted over time.
    
    Signed-off-by: Phillip Wood <phillip.wood@dunelm.org.uk>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -127,10 +127,17 @@ then
 	export GIT_TEST_DISALLOW_ABBREVIATED_OPTIONS
 fi
 
-# Explicitly set the default branch name for testing, to avoid the
-# transitory "git init" warning under --verbose.
-: ${GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME:=master}
-export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
+if test -z "$WITH_BREAKING_CHANGES"
+then
+	# Explicitly set the default branch name for testing, to avoid the
+	# transitory "git init" warning under --verbose.
+	: ${GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME:=master}
+	export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
+else
+	# Unset the default branch name that is set in many test scripts
+	# before sourcing this file.
+	unset GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
+fi
 
 ################################################################
 # It appears that people try to run tests without building...
